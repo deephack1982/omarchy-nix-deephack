e95d780fe7acc0666114e316bb23322a8720ae2d
Mark Dickie <mark.dickie@gmail.com>
2025-08-18 16:00:05 +0200

    Correct syntax

--- modules/home-manager/pyprland.nix
+++ modules/home-manager/pyprland.nix
@@ -5,16 +5,16 @@
 }: {
   home.file = {
     ".config/hyprland/pyprland.toml" = {
-    text = ''
-      [pyprland]
-      plugins = [ "scratchpads" ]
+      text = ''
+        [pyprland]
+        plugins = [ "scratchpads" ]
 
-      [scratchpads.term]
-      command = "ghostty"
-      class = "com.mitchellh.ghostty"
-      size = "80% 40%"
-      multi = false
-    ''
+        [scratchpads.term]
+        command = "ghostty"
+        class = "com.mitchellh.ghostty"
+        size = "80% 40%"
+        multi = false
+      '';
     };
   };
 }
