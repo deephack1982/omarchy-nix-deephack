e09fab57ee8641018b46371141b72c0fca486253
Mark Dickie <mark.dickie@gmail.com>
2025-08-20 15:26:56 +0200

    Try to change the clipboard startup

--- modules/home-manager/hyprland/autostart.nix
+++ modules/home-manager/hyprland/autostart.nix
@@ -7,7 +7,8 @@
     exec-once = [
       "hyprsunset"
       "systemctl --user start hyprpolkitagent"
-      "wl-clip-persist --clipboard regular & clipse -listen"
+      "wl-clip-persist --clipboard regular"
+      "clipse -listen"
       "mkfifo -m 600 /tmp/wobpipe"
       "tail -f /tmp/wobpipe | wob"
       "pypr --debug /tmp/pypr.log"
