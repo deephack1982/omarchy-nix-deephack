7b429674364595bd827f6ed7eb00e774a7b49bb7
Mark Dickie <mark.dickie@gmail.com>
2025-10-07 16:18:21 +0200

    Change binds

--- modules/home-manager/hyprland/bindings.nix
+++ modules/home-manager/hyprland/bindings.nix
@@ -85,8 +85,8 @@ in {
 
         # Grouped windows binds
         "SUPER, Tab, changegroupactive, f"
-        "SUPER, left, movewindoworgroup, left"
-        "SUPER, right, movewindoworgroup, right"
+        "SUPER ALT, left, movewindoworgroup, left"
+        "SUPER ALT, right, movewindoworgroup, right"
 
         # Scratchpad binds
         "SUPER, grave, exec, pypr toggle term"
