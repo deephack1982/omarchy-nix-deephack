fd4949af0aab803ce5adb5c524819e37bce1b9bd
Mark Dickie <mark.dickie@gmail.com>
2025-10-16 09:33:21 +0200

    Adjust kitty colours

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -13,6 +13,10 @@ in {
     settings = {
       font_family = cfg.primary_font;
       font_size = 14;
+      cursor_trail = 10;
+      cursor_trail_decay = "0.1 0.4";
+      cursor_trail_start_threshold = 2;
+      cursor_trail_color = "orange";
     };
   };
 }
