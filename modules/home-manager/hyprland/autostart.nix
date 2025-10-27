2bce6284d208bcafdb27e56b74277d817d994d07
Mark Dickie <mark.dickie@gmail.com>
2025-10-27 13:16:29 +0100

    Adjust autostart

--- modules/home-manager/hyprland/autostart.nix
+++ modules/home-manager/hyprland/autostart.nix
@@ -4,11 +4,11 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "uwsm-app -- hyprsunset"
-      "uwsm-app -- wl-clip-persist --clipboard regular"
-      "uwsm-app -- clipse -listen"
+      "wl-clip-persist --clipboard regular"
+      "clipse -listen"
       "mkfifo -m 600 /tmp/wobpipe"
       "tail -f /tmp/wobpipe | wob"
-      "uwsm-app -- pypr --debug /tmp/pypr.log"
+      "pypr --debug /tmp/pypr.log"
       "uwsm-app -- keepassxc --minimized"
       "uwsm-app -- waybar"
     ];
